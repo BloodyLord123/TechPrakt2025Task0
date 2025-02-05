e24f7300f9a8a0117378ef3b6e19ae110b407a16
AisinVlad <ipz231_avs@student.ztu.edu.ua>
2025-02-05 23:17:34 +0200

    Console app and gitignore

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+            Console.WriteLine(a+b);
+        }
+    }
+}
